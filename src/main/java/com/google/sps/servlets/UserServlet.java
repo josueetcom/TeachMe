8ccfc3c55f5ccbe877345d3e0e5fbc144fbff237
Josiah <jopaintsil@gmail.com>
2021-04-02 21:32:56 +0000

    Working datastore submission form for user profiles

--- src/main/java/com/google/sps/servlets/UserServlet.java
+++ src/main/java/com/google/sps/servlets/UserServlet.java
@@ -5,16 +5,18 @@ import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Key;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.io.*;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
 @WebServlet("/users")
-public class UserServlet extends HttpServlet{
+public class UserServlet extends HttpServlet {
 
     @Override
-    public void doPost() { // create
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException { // create
 
         // get form information
         String name = Jsoup.clean(request.getParameter("name"), Whitelist.none());
@@ -22,9 +24,30 @@ public class UserServlet extends HttpServlet{
         String bio = Jsoup.clean(request.getParameter("bio"), Whitelist.none());
         String wishlist = Jsoup.clean(request.getParameter("wishlist"), Whitelist.none());
         String socialInfo = Jsoup.clean(request.getParameter("socialInfo"), Whitelist.none());
+
+        // create new datastore and keyfactory 
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory();
+
+        // add kind users to keyfactory
+        keyFactory.setKind("users");
+
+        // take the user elements and create a new entity object for it 
+        FullEntity userEntity =
+          Entity.newBuilder(keyFactory.newKey())
+              .set("name", name)
+              .set("email", email)
+              .set("bio", bio)
+              .set("wishlist", wishlist)
+              .set("socialInfo", socialInfo)
+              .build();
+        
+        // insert that new entity
+        datastore.add(userEntity);
+        response.sendRedirect("index.html");
     }
 
-    @Override
+    /*@Override
     public void doGet() { // read
 
 
@@ -39,5 +62,5 @@ public class UserServlet extends HttpServlet{
     @Override
     public void doDelete() { // delete
 
-    }
+    }*/
 }
