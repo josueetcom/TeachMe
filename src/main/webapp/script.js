b48fe876da88c4aee7a7fcd3697ad27e1c5517b3
Jose <A01023661@itesm.mx>
2021-04-08 21:33:50 -0500

    Redesigned the Website

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,15 +1,32 @@
+var counter = 0;
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-  window.location.replace("dashboard.html");
+  console.log('ID Token: ' + id_token);
+
+  document.getElementById('user-profile').innerHTML = profile.getGivenName();
+  document.getElementById('headert').innerHTML =
+    profile.getGivenName() + ' ' + profile.getFamilyName();
+  document.getElementById('email').innerHTML = profile.getEmail();
+}
+
+function Switch() {
+  if (counter == 0) {
+    document.getElementById('content-userprofile').style.display = 'flex';
+    document.getElementById('content-dashboard').style.display = 'none';
+    counter += 1;
+  } else {
+    document.getElementById('content-userprofile').style.display = 'none';
+    document.getElementById('content-dashboard').style.display = '';
+    counter = 0;
+  }
 }
