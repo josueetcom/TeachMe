0d20dbe80e6c8d3d6f8ff42161129332303ef1ff
Obi <straightup.stack@gmail.com>
2021-04-10 03:38:37 +0000

    Errors with the chat servlet have been fixed and the server runs as expected. Moving on to getting \the queries right and rereturning the info to the webpage in json form

--- src/main/java/com/google/sps/data/Chat.java
+++ src/main/java/com/google/sps/data/Chat.java
@@ -1,15 +1,29 @@
-package main.java.com.google.sps.data;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
+package com.google.sps.data;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.Chat;
+
+import java.io.IOException;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import java.lang.String;
-import java.lang.Long;
 
 /** An item on a todo list. */
 public final class Chat {
@@ -26,41 +40,45 @@ public final class Chat {
   }
 
   // //Constructor 2
-  // Chat(Entity entity) {
-  //   this.id = (long) entity.getKey.getId();
-  //   this.participants = (String) entity.getProperty("participants");
-  //   this.messages = entity.getProperty("messages");
-  // }
+  Chat(Entity entity) {
+    this.id = (long) entity.getKey().getId();
+    this.participants = entity.getString("participants");
+    // this.messages = entity.getProperty("messages");
+  }
 
-  // public static List<Chat> getChat(DatastoreService datastore, long chatid) {
-  //   Key key = KeyFactory.createKey("Chat", chatid);
+  public static Chat getChat(Datastore datastore, String chatid) {
+    Key key = datastore.newKeyFactory().setKind("Chat").newKey(chatid);
 
-  //   try {
-  //     Entity entity = datastore.get(key);
-  //     Chat c = new Chat(entity);
-  //     return c;
-  //   } catch (EntityNotFoundException e) {
-  //     return null;
-  //   }
-  // }
+    try {
+      Entity entity = datastore.get(key);
+      Chat c = new Chat(entity);
+      return c;
+    } catch (Exception e) {
+      return null;
+    }
+  }
 
-  // public static List<Chat> getChats(DatastoreService datastore, long participantid) {
-  //   List<Chat> chats = new ArrayList<>();
-  //   Query chatQuery = new Query("Chat");
-  //   PreparedQuery chatEntities = datastore.prepare(chatQuery);
+  public static List<Chat> getChats(Datastore datastore, String participantid) {
+    
+    List<Chat> chats = new ArrayList<>();
+    Query<Entity> chatQuery = Query
+    .newEntityQueryBuilder()
+    .setKind("Chat")
+    .build();
+    QueryResults<Entity> chatEntities = datastore.run(chatQuery);
 
-  //   // Iterate through the chats
-  //   for (Entity entity : chatEntities.asIterable()) {
-  //     long chatid = entity.getKey.getId();
-  //     List<String> participantList = Arrays.asList(entity.getProperty("participants").split(","));
+    // Iterate through the chats
+    // for (Entity entity : chatEntities.asIterable()) {
+    //   long chatid = entity.getKey.getId();
+    //   List<String> participantList = Arrays.asList(entity.getProperty("participants").split(","));
 
-  //     if (participantList.contains(Long.toString(participantid))){
-  //       Chat chat = new Chat(entity);
-  //       chats.add(chat);
-  //     }
-  //   }
-  //   return chats;
-  // }
+    //   if (participantList.contains(participantid)){
+    //     Chat chat = new Chat(entity);
+    //     chats.add(chat);
+    //   }
+    // }
+    return chats;
+  }
 
 
   long getId(){
